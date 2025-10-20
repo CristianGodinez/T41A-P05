567127ed718e88bc9b40d8789131f5acfe1a0b90
Jesús Alberto Revilla Silva <jesus_revilla@yahoo.com>
2025-10-20 17:29:49 -0600

    Add comments to 03_query_data.sql
    Added comments for various SQL queries.

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,19 @@
+-- Listar todos los gastos con información del usuario, tipo de gasto y proveedor
 
+
+
+-- Total gastado por cada departamento
+
+
+
+
+-- Total de gastos por día
+
+
+
+-- Buscar proveedores cuyo nombre contiene 'Tech'
+
+
+
+
+-- Mostrar nombre de usuario en mayúsculas
